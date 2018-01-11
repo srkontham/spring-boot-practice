d2bc5b753950b2fd4d0a156dcb4532b39675fa13
Koushik <Koushik@Koushik-PC>
2018-01-12 00:21:02 +0530

    Spring starter project initial commit

---
+++ spring-boot-course-api-data/src/main/java/com/srk/springbootstarter/SpringBootCourseApiDataApplication.java
@@ -0,0 +1,12 @@
+package com.srk.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseApiDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCourseApiDataApplication.class, args);
+	}
+}
---
+++ spring-boot-course-api-data/src/test/java/com/srk/springbootstarter/SpringBootCourseApiDataApplicationTests.java
@@ -0,0 +1,16 @@
+package com.srk.springbootstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootCourseApiDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
