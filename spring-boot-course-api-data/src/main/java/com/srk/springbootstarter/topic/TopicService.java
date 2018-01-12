1fbbd025de3adb081f5e1afe74933ca4f3e740a9
sreddyk <sreddyk@DIN67000580.corp.capgemini.com>
2018-01-12 18:44:25 +0530

    Course API with JPA implementation

--- spring-boot-course-api-data/src/main/java/com/srk/springbootstarter/topic/TopicService.java
+++ spring-boot-course-api-data/src/main/java/com/srk/springbootstarter/topic/TopicService.java
@@ -3,39 +3,41 @@ package com.srk.springbootstarter.topic;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-
+	
+	@Autowired
 	private TopicRepository topicRepository;
-	
-	
-//	private List<Topic> topics = new ArrayList<>(Arrays.asList(
-//			new Topic("spring", "Spring Framework", "Spring Framework Description"),
-//			new Topic("java", "Core Java", "Core Java Description"),
-//			new Topic("javascript", "Java Script", "Java Script Description")
-//			));
-//	
-	public List<Topic> getAllTopics(){
+
+	// private List<Topic> topics = new ArrayList<>(Arrays.asList(
+	// new Topic("spring", "Spring Framework", "Spring Framework Description"),
+	// new Topic("java", "Core Java", "Core Java Description"),
+	// new Topic("javascript", "Java Script", "Java Script Description")
+	// ));
+	//
+	public List<Topic> getAllTopics() {
 		List<Topic> topics = new ArrayList<>();
 		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
 
+	public Topic getTopic(String id) {
+		return topicRepository.findOne(id);
+	}
+
 	public void addTopic(Topic topic) {
 		topicRepository.save(topic);
 	}
-	
+
 	public void updateTopic(String id, Topic topic) {
 		topicRepository.save(topic);
 	}
-	
+
 	public void deleteTopic(String id) {
 		topicRepository.delete(id);
 	}
-	
-//	public Topic getTopic(String id) {
-//		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-//	}
+
 }
